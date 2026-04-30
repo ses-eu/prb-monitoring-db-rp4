d2ef9b04a0376ac57d147a8be63f5cb1a9b11571
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2026-04-30 07:45:53 +0200

    included Darios changes in qmds

--- R/chart_level2_env_vfe_ert.R
+++ R/chart_level2_env_vfe_ert.R
@@ -7,20 +7,20 @@ if (!exists("data_loaded")) {
 data_raw <- vfe_ert_actual
 
 ## prepare data ----
-data_prep <- data_raw %>% 
+data_prep <- data_raw %>%
   filter(
     area_id == .env$state_icao_vef_code,
     year == .env$year_report
   ) %>%
   mutate(
-    mymetric = round(vfe_ert*100 , 1),
+    mymetric = round(vfe_ert * 100, 1),
     type = "En route VFE"
-  ) %>% 
+  ) %>%
   select(
     xlabel = year,
     type,
     mymetric
-  ) 
+  )
 
 ## chart parameters ----
 c_suffix <- "%"
@@ -39,32 +39,32 @@ c_textposition <- "inside"
 c_insidetextanchor <- "middle"
 
 #### title
-c_title_text <- paste0("Vertical en-route flight efficiency")
+c_title_text <- paste0("Vertical flight efficiency of the actual trajectory")
 
 #### yaxis
-c_yaxis_title <- "Vertical en-route flight efficiency (%)"
-c_yaxis_tickformat <- paste0(".",0, "f")
+c_yaxis_title <- "Vertical flight efficiency (%)"
+c_yaxis_tickformat <- paste0(".", 0, "f")
 
 ## plot chart  ----
-myplot <- mybarchart2(data_prep, 
-                      height = myheight,
-                      colors = c_colors,
-                      local_factor = c_factor,
-                      suffix = c_suffix,
-                      decimals = c_decimals,
-                      
-                      hovertemplate = c_hovertemplate,
-                      
-                      textangle = c_textangle,
-                      textposition = c_textposition,
-                      insidetextanchor = c_insidetextanchor,
-                      
-                      title_text = c_title_text,
-                      
-                      yaxis_title = c_yaxis_title,
-                      yaxis_ticksuffix = c_suffix,
-                      yaxis_tickformat = c_yaxis_tickformat
+myplot <- mybarchart2(
+  data_prep,
+  height = myheight,
+  colors = c_colors,
+  local_factor = c_factor,
+  suffix = c_suffix,
+  decimals = c_decimals,
+
+  hovertemplate = c_hovertemplate,
+
+  textangle = c_textangle,
+  textposition = c_textposition,
+  insidetextanchor = c_insidetextanchor,
+
+  title_text = c_title_text,
+
+  yaxis_title = c_yaxis_title,
+  yaxis_ticksuffix = c_suffix,
+  yaxis_tickformat = c_yaxis_tickformat
 )
 
-myplot %>% layout(xaxis=list(range = c(rp_min_year-0.5,rp_max_year+0.5)))
-
+myplot %>% layout(xaxis = list(range = c(rp_min_year - 0.5, rp_max_year + 0.5)))
