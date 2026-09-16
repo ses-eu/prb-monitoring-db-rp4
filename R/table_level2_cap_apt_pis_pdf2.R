0a0770fc18741382be3c67fadad6ce1936d2c575
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2026-09-16 16:59:01 +0200

    filter small airports from cap pi table 2 pdf

--- R/table_level2_cap_apt_pis_pdf2.R
+++ R/table_level2_cap_apt_pis_pdf2.R
@@ -5,53 +5,64 @@ if (!exists("data_loaded")) {
 
 data_raw <- cap_apt_pis_actual
 
-## prepare data ----
-data_prep <- data_raw %>%
+big_airports <- airports_table |>
   filter(
-    state == .env$country
-  ) %>%
-  mutate_at(
-    vars(-one_of(c('year', 'airport_icao'))),
-    ~ ifelse(year > year_report, NA, .)
-  ) %>%
-  filter(airport_icao %in% airports_table$apt_code) %>%
-  left_join(airports_table, by = c("airport_icao" = "apt_code")) %>%
-  arrange(apt_name) %>%
-  rename("Airport name" = apt_name) %>%
-  mutate(
-    "Avg arrival\nATFM delay (KPI#2)" = format(
-      janitor::round_half_up(terminal_delay, 2),
-      decimals = 2
-    ),
-    "Slot adherence (PI#1)" = paste0(
-      format(janitor::round_half_up(slot_adherence * 100, 1), decimals = 1),
-      "%"
-    ),
-    "ATC pre departure\ndelay (PI#2)" = format(
-      janitor::round_half_up(atc_predep_dly, 2),
-      decimals = 2
-    ),
-    "All causes pre departure\ndelay (PI#3)" = format(
-      janitor::round_half_up(all_cause_predep_dly, 1),
-      decimals = 1
-    ),
-  ) %>%
-  select(
-    year,
-    "Airport name",
-    # , "Avg arrival\nATFM delay (KPI#2)"
-    # , "Slot adherence (PI#1)"
-    "ATC pre departure\ndelay (PI#2)",
-    "All causes pre departure\ndelay (PI#3)"
-  ) %>%
-  pivot_wider(
-    names_from = "year",
-    values_from = c(
-      # "Avg arrival\nATFM delay (KPI#2)"
+    country_name == .env$country,
+    big == 1
+  ) |>
+  select(apt_code) |>
+  pull()
+
+## prepare data ----
+if (no_apt_big != 0) {
+  data_prep <- data_raw %>%
+    filter(
+      state == .env$country,
+      airport_icao %in% big_airports
+    ) %>%
+    mutate_at(
+      vars(-one_of(c('year', 'airport_icao'))),
+      ~ ifelse(year > year_report, NA, .)
+    ) %>%
+    filter(airport_icao %in% airports_table$apt_code) %>%
+    left_join(airports_table, by = c("airport_icao" = "apt_code")) %>%
+    arrange(apt_name) %>%
+    rename("Airport name" = apt_name) %>%
+    mutate(
+      "Avg arrival\nATFM delay (KPI#2)" = format(
+        janitor::round_half_up(terminal_delay, 2),
+        decimals = 2
+      ),
+      "Slot adherence (PI#1)" = paste0(
+        format(janitor::round_half_up(slot_adherence * 100, 1), decimals = 1),
+        "%"
+      ),
+      "ATC pre departure\ndelay (PI#2)" = format(
+        janitor::round_half_up(atc_predep_dly, 2),
+        decimals = 2
+      ),
+      "All causes pre departure\ndelay (PI#3)" = format(
+        janitor::round_half_up(all_cause_predep_dly, 1),
+        decimals = 1
+      ),
+    ) %>%
+    select(
+      year,
+      "Airport name",
+      # , "Avg arrival\nATFM delay (KPI#2)"
       # , "Slot adherence (PI#1)"
       "ATC pre departure\ndelay (PI#2)",
       "All causes pre departure\ndelay (PI#3)"
-    )
-    # , names_glue = "{year}_{.value}" #suffix to prefix
-  ) %>%
-  mutate(across(everything(), ~ str_replace_all(.x, fixed("NA%"), "NA")))
+    ) %>%
+    pivot_wider(
+      names_from = "year",
+      values_from = c(
+        # "Avg arrival\nATFM delay (KPI#2)"
+        # , "Slot adherence (PI#1)"
+        "ATC pre departure\ndelay (PI#2)",
+        "All causes pre departure\ndelay (PI#3)"
+      )
+      # , names_glue = "{year}_{.value}" #suffix to prefix
+    ) %>%
+    mutate(across(everything(), ~ str_replace_all(.x, fixed("NA%"), "NA")))
+}
